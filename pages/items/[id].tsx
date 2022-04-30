bf87b80de0fe8295285c9cd88cbeaeb5008c6fd3
rexiann <gandolkim@gmail.com>
2022-04-30 14:02:32 +0900

    #6.0 components separated. baseUrl and path added

--- pages/items/[id].tsx
+++ pages/items/[id].tsx
@@ -1,31 +1,26 @@
-import * as React from "react";
 import type { NextPage } from "next";
-
-import Layout from "../../components/layout";
+import Button from "@/components/Button";
+import Layout from "@/components/Layout";
 
 const ItemDetail: NextPage = () => {
   return (
-    <Layout canGoBack={true}>
-      <div className={"px-4 py-10"}>
-        <div className={"mb-8"}>
-          <div className={"h-96 bg-slate-300"} />
-          <div
-            className={
-              "flex  cursor-pointer items-center space-x-3 border-b border-t py-3"
-            }
-          >
-            <div className={"h-12 w-12 rounded-full bg-slate-300"} />
+    <Layout canGoBack>
+      <div className="px-4  py-4">
+        <div className="mb-8">
+          <div className="h-96 bg-slate-300" />
+          <div className="flex cursor-pointer items-center space-x-3 border-t border-b py-3">
+            <div className="h-12 w-12 rounded-full bg-slate-300" />
             <div>
-              <p className={"text-sm font-medium text-gray-700"}>Steve Jobs</p>
-              <p className={"text-xs font-medium text-gray-500"}>
+              <p className="text-sm font-medium text-gray-700">Steve Jebs</p>
+              <p className="text-xs font-medium text-gray-500">
                 View profile &rarr;
               </p>
             </div>
           </div>
-          <div className={"mt-5"}>
-            <h1 className={"text-3xl font-bold text-gray-900"}>Galaxy S50</h1>
-            <span className={"mt-3 block text-3xl text-gray-900"}>$140</span>
-            <p className={"my-6 text-base text-gray-700"}>
+          <div className="mt-5">
+            <h1 className="text-3xl font-bold text-gray-900">Galaxy S50</h1>
+            <span className="mt-3 block text-2xl text-gray-900">$140</span>
+            <p className=" my-6 text-gray-700">
               My money&apos;s in that office, right? If she start giving me some
               bullshit about it ain&apos;t there, and we got to go someplace
               else and get it, I&apos;m gonna shoot you in the head then and
@@ -35,22 +30,9 @@ const ItemDetail: NextPage = () => {
               in there, and that ni**a Winston or anybody else is in there, you
               the first motherfucker to get shot. You understand?
             </p>
-            <div className={"flex items-center justify-between space-x-2"}>
-              <button
-                className={
-                  "flex-1 rounded-md bg-orange-500 py-3 text-white " +
-                  "font-medium shadow-md focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 " +
-                  "hover:bg-orange-600"
-                }
-              >
-                Talk to seller
-              </button>
-              <button
-                className={
-                  "flex items-center justify-center p-3 text-gray-400 hover:bg-gray-100 hover:text-gray-500 " +
-                  "rounded-md"
-                }
-              >
+            <div className="flex items-center justify-between space-x-2">
+              <Button large text="Talk to seller" />
+              <button className="flex items-center justify-center rounded-md p-3 text-gray-400 hover:bg-gray-100 hover:text-gray-500">
                 <svg
                   className="h-6 w-6 "
                   xmlns="http://www.w3.org/2000/svg"
@@ -71,13 +53,13 @@ const ItemDetail: NextPage = () => {
           </div>
         </div>
         <div>
-          <h2 className={"text-2xl font-bold text-gray-900"}>Similar items</h2>
-          <div className={"mt-6 grid grid-cols-2 gap-4"}>
+          <h2 className="text-2xl font-bold text-gray-900">Similar items</h2>
+          <div className=" mt-6 grid grid-cols-2 gap-4">
             {[1, 2, 3, 4, 5, 6].map((_, i) => (
               <div key={i}>
-                <div className={"mb-4 h-56 w-full bg-slate-300"} />
-                <h3 className={"-mb-1 text-gray-700"}>Galaxy S60</h3>
-                <span className={"text-sm font-medium text-gray-900"}>$6</span>
+                <div className="mb-4 h-56 w-full bg-slate-300" />
+                <h3 className="-mb-1 text-gray-700">Galaxy S60</h3>
+                <span className="text-sm font-medium text-gray-900">$6</span>
               </div>
             ))}
           </div>
@@ -86,5 +68,4 @@ const ItemDetail: NextPage = () => {
     </Layout>
   );
 };
-
 export default ItemDetail;
