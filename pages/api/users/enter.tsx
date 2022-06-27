f3c9dfa6d6999c58051be4997006aa10a83fce9e
rexiann <gandolkim@gmail.com>
2022-06-27 23:03:35 +0900

    #9.1~9.2 upsert

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -4,45 +4,20 @@ import withHandler from "@/libs/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
-  let user;
+  const payload = phone ? { phone: +phone } : { email };
 
-  if (email) {
-    user = await client.user.findUnique({
-      where: {
-        email,
-      },
-    });
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
 
-    if (user == null) {
-      console.log("user not found. will create");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          email,
-        },
-      });
-    }
-  }
-
-  if (phone) {
-    user = await client.user.findUnique({
-      where: {
-        phone: +phone,
-      },
-    });
-
-    if (user == null) {
-      console.log("user not found. will create");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          phone: +phone,
-        },
-      });
-    }
-  }
-
-  console.log({ user });
+  console.log(user);
   return res.status(200).end();
 }
 
