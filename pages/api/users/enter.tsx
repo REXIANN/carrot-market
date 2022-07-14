135e79a32c91509ef904a7d14f7191f0e475767d
rexiann <gandolkim@gmail.com>
2022-07-14 23:35:34 +0900

    #9.3 twilio setup

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,11 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next";
+
 import client from "@/libs/server/client";
 import withHandler from "@/libs/server/withHandler";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+import { ResponseType } from "@/libs/server/types";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { phone, email } = req.body;
   const user = phone ? { phone: +phone } : email ? { email } : null;
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
+
+  if (user == null) {
+    return res.status(400).json({ ok: false });
+  }
 
   const token = await client.token.create({
     data: {
@@ -25,7 +35,9 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   });
 
   console.log(token);
-  return res.status(200).end();
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler("POST", handler);
